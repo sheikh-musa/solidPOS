2977beb2e4c1e95d5f05e8e6926a6cd44ba6fe85
sheikh-musa <97861619+sheikh-musa@users.noreply.github.com>
2023-04-06 12:50:04 +0800

    chatgpt troubleshoot

--- src/routes/pos.tsx
+++ src/routes/pos.tsx
@@ -6,7 +6,6 @@ import { ShoppingCart } from "../components/Cart";
 export default function POS() {
 	return (
 		<main class="text-center mx-auto text-gray-700 p-4">
-			<h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">POS Page</h1>
 			<Products />
 			<ShoppingCart />
 			{/* <Counter /> */}
